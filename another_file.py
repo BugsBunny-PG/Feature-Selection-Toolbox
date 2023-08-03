630ca3c81a996fd3e15da8ed133cf135f36a728f
BugsBunny-PG <gupta.pragya433@gmail.com>
2023-08-03 23:10:46 +0530

    add initial file

---
+++ another_file.py
@@ -0,0 +1,61 @@
+import pandas as pd
+import shutil
+import nbformat as nbf
+from nbconvert.preprocessors import ExecutePreprocessor
+import os
+
+def perform_feature_selection(file,algo):
+    # Create a directory for storing uploaded files
+    upload_folder = "uploaded_files"
+    os.makedirs(upload_folder, exist_ok=True)
+
+    # Save the uploaded file in the folder with a specific name
+    uploaded_file_name = os.path.join(upload_folder, "uploaded_file.csv")
+    with open(uploaded_file_name, "wb") as f:
+        f.write(file.read())
+    if algo=='Genetic Algorithm':
+        result=execute_notebook("multiobjectiveGeneticAlgorithm.ipynb", uploaded_file_name)
+        print("Yes")
+    else:
+       # Run testBBO2.ipynb through nbconvert and get the result CSV path
+       result = execute_notebook("testBBO2.ipynb", uploaded_file_name)
+
+    # Read the result CSV file
+    # result = pd.read_csv(result_file)
+
+    # Return the result
+    return result
+
+def execute_notebook(notebook_file, input_file):
+    # Read the notebook
+    with open(notebook_file, "r") as f:
+        notebook = nbf.read(f, as_version=nbf.NO_CONVERT)
+
+    # Modify the input file path in the notebook
+    for cell in notebook.cells:
+        if "uploaded_file.csv" in cell.source:
+            cell.source = cell.source.replace("uploaded_file.csv", input_file)
+
+    # Execute the notebook
+    ep = ExecutePreprocessor()
+    ep.preprocess(notebook)
+
+    # Find the result CSV file path in the notebook outputs
+    result_file = pd.read_csv("result.csv")
+    return result_file
+
+# if __name__ == "__main__":
+#     file = pd.read_csv("your_uploaded_file.csv")  # Replace with your uploaded file
+#     result_file = perform_feature_selection(file)
+
+#     if result_file is not None:
+#         result = pd.read_csv(result_file)
+#         print(result)
+
+
+
+
+
+
+
+
---
+++ main.py
@@ -0,0 +1,115 @@
+import streamlit as st
+import base64
+from another_file import perform_feature_selection
+from io import BytesIO
+
+# Custom CSS styles
+st.markdown(
+    """
+    <style>
+    body {
+        font-family: Arial, sans-serif;
+        margin-top: 10px;
+        margin-bottom: 10px;
+    }
+    .header {
+        font-size: 36px;
+        font-weight: bold;
+        color: #2e86de;
+        margin-bottom: 10px;
+    }
+    .upload-container {
+        display: flex;
+        justify-content: center;
+    }
+    .upload-box {
+        width: 70%;
+        padding: 20px;
+        border: 2px dashed #2e86de;
+        border-radius: 10px;
+        text-align: center;
+        color: #2e86de;
+        font-size: 20px;
+        cursor: pointer;
+    }
+    .selected-file {
+        margin-top: 10px;
+        font-size: 18px;
+        font-weight: bold;
+    }
+    .algorithm-selector {
+        margin-top: 20px;
+    }
+    .button-container {
+        display: flex;
+        justify-content: center;
+        margin-top: 20px;
+        margin-bottom: 20px;
+    }
+    .button-style {
+        background-color: #22a3a0; /* Change the button color to green */
+        color: #fff;
+        padding: 12px 24px;
+        font-size: 18px;
+        font-weight: bold;
+        border: none;
+        border-radius: 4px;
+        cursor: pointer;
+        margin: 0 10px;
+        transition: background-color 0.3s ease;
+    }
+    .button-style:hover {
+        background-color: #F8F8FF; /* Change the hover color to a darker shade of red */
+    }
+    .result {
+        padding: 20px;
+        border-radius: 8px;
+        box-shadow: 0 0 8px rgba(0, 0, 0, 0.2);
+        overflow-x: auto;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+def main():
+    st.title("Feature Selection Toolbox")
+
+    # Add an image to the website
+    st.image("https://media.licdn.com/dms/image/D4D12AQEvmbuFNHsobA/article-cover_image-shrink_600_2000/0/1687623875560?e=2147483647&v=beta&t=DKZ9ILeRBD4L2SfR5ro3RILV210nMH89rJbXjiTart0", use_column_width=True)
+
+    st.markdown(
+        "Welcome to the Feature Selection Toolbox, a powerful tool for optimizing your dataset. Upload a CSV file with integer, real, or categorical values (no text or nulls) and place the target column on the right. Choose from various algorithms for feature selection, view results, and download selected features as a CSV file. Trust our guidelines for accurate and reliable outcomes. Empower your data analysis with this user-friendly and efficient toolbox"
+    )
+
+    # File selection
+    st.markdown("<div class='header'>File Selection</div>", unsafe_allow_html=True)
+    uploaded_file = st.file_uploader(" ", type=["csv"], accept_multiple_files=False, key="file_uploader")
+    if uploaded_file:
+        st.markdown("<div class='selected-file'>Selected File: " + uploaded_file.name + "</div>", unsafe_allow_html=True)
+
+    # Algorithm selection
+    st.markdown("<div class='header algorithm-selector'>Algorithm Selection</div>", unsafe_allow_html=True)
+    algorithms = ["Genetic Algorithm", "Biogeography-Based Optimization Algorithm"]
+    selected_algorithm = st.selectbox("Select an algorithm", algorithms)
+
+    # Perform feature selection
+    if st.button("Perform Feature Selection", key="perform_btn"):
+        if uploaded_file is None:
+            st.warning("Please upload a file.")
+        else:
+            # Send the uploaded file to the another_file.py file
+            result = perform_feature_selection(uploaded_file,selected_algorithm)
+
+            # Display the result
+            st.markdown("<div class='header result'>Feature Selection Result</div>", unsafe_allow_html=True)
+            st.dataframe(result)
+
+            # Download button for the result file
+            csv_file = result.to_csv(index=False)
+            b64 = base64.b64encode(csv_file.encode()).decode()
+            href = f'<a class="button-style" href="data:file/csv;base64,{b64}" download="result.csv">Download Result CSV</a>'
+            st.markdown(href, unsafe_allow_html=True)
+
+if __name__ == "__main__":
+    main()
